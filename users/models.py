ec97463f6a50d5316e56bf147ffea50441442ced
Pavel Artiev <arijev@bk.ru>
2025-07-28 16:54:50 +0300

    Payment and PaymentMethod model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from course.models import Course, Lesson
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -20,3 +22,38 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class PaymentMethod(models.Model):
+    name = models.CharField(max_length=20, verbose_name="Вид оплаты")
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    payment_date = models.DateField(null=True, blank=True, verbose_name="Дата платежа")
+    payed_course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Оплаченный курс",
+    )
+    payed_lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Отдельно оплаченный урок",
+    )
+    payment_amount = models.IntegerField(
+        null=True, blank=True, verbose_name="Сумма платежа"
+    )
+    payment_method = models.ForeignKey(
+        PaymentMethod,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Вид оплаты",
+    )
