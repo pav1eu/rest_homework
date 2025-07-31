b6b21941e5a0b9e4094587a496693e5f10740f10
Pavel Artiev <arijev@bk.ru>
2025-07-31 21:27:46 +0300

    url changes

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("course/", include("course.urls", namespace="course")),
-    path("payment/", include("users.urls", namespace="payment")),
+    path("users/", include("users.urls", namespace="users")),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
-from users.views import PaymentListView
+from users.views import PaymentListView, UserCreateAPIView, UserListView, UserDetailView
 
 from users.apps import UsersConfig
 
@@ -9,5 +9,10 @@ app_name = UsersConfig.name
 
 
 urlpatterns = [
-    path("", PaymentListView.as_view(), name="payment-list"),
+    path("payment/", PaymentListView.as_view(), name="payment-list"),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('', UserListView.as_view(), name='user_list'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
