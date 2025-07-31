ce41b9453da29e54da9a9faa79e295743b3e0188
Pavel Artiev <arijev@bk.ru>
2025-07-31 21:27:32 +0300

    permissions

--- users/serializer.py
+++ users/serializer.py
@@ -1,8 +1,15 @@
-from users.models import Payment
+from users.models import Payment, User
 from rest_framework import serializers
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        # fields = ['id', 'email', 'phone', 'city', 'avatar']
         fields = "__all__"
