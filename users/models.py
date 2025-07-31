3f3934b6849c8e874e477e522d6cae0f938e81f7
Pavel Artiev <arijev@bk.ru>
2025-07-31 21:27:26 +0300

    def __str__

--- users/models.py
+++ users/models.py
@@ -19,6 +19,9 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
+    def __str__(self):
+        return self.email
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
