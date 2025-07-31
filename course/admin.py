3df5406afb40c306d8e3a3a020b584d8fd93d615
Pavel Artiev <arijev@bk.ru>
2025-07-31 21:25:14 +0300

    admin models

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from course.models import Course, Lesson
+
+
 # Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("id", "title",)
+    search_fields = ("title",)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("id", "title",)
+    list_filter = ("title",)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User, Payment
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ("email",)
+    list_display = ("email",)
+    search_fields = ("email",)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('phone', 'city', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("user", "payed_course", "payed_lesson", "payment_method")
+    list_filter = ("user",)
+
