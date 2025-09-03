81798797929269d8347dfab28bb27349669bea38
Pavel Artiev <arijev@bk.ru>
2025-09-03 11:37:16 +0300

    Небольшие изменения валидатора

--- course/validators.py
+++ course/validators.py
@@ -5,8 +5,17 @@ class YoutubeOnlyValidator:
     def __init__(self, field):
         self.field = field
 
-    def __call__(self, value):
-        parsed_url = urlparse(value)
+    def __call__(self, attrs):
+        url_value = None
+        if isinstance(attrs, dict):
+            url_value = attrs.get(self.field)
+        else:
+            url_value = attrs
+
+        if not url_value:
+            return
+
+        parsed_url = urlparse(url_value)
         domain = parsed_url.netloc.lower()
         if 'youtube.com' not in domain:
             raise ValidationError({self.field: "Разрешены только ссылки на YouTube."})
